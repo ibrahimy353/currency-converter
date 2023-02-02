b1f211e936534e7c90b08eed9a33b3f2457dea6e
ibrahimy353 <ibrahimy353@gmail.com>
2023-02-02 23:42:50 +0300

    modify

--- currency converter/src/main.jsx
+++ currency converter/src/main.jsx
@@ -1,13 +1,12 @@
+import {QuerClientProvider} from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-const queryClient = new QueryClient()
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client>
+    <QuerClientProvider>
     <App />
-    </QueryClientProvider>
+    </QuerClientProvider>
   </React.StrictMode>,
-)
+);
