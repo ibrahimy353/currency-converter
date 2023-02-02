0ea134a81c266a31e7595d92da082ebcffa701a8
ibrahimy353 <ibrahimy353@gmail.com>
2023-02-02 23:15:13 +0300

    imported query client

--- currency converter/src/main.jsx
+++ currency converter/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <QueryClientProvider client>
     <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
