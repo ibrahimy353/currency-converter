199b2983cfe63ea5b537d3ab4a8a1923d19273be
ibrahimy353 <ibrahimy353@gmail.com>
2023-02-03 17:22:42 +0300

    added reactquery devtools that enables us to view the development from inside the site

--- currency converter/src/main.jsx
+++ currency converter/src/main.jsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import {QuerClientProvider, QueryClient} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
@@ -12,8 +13,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <QuerClientProvider cleint ={queryClient}>
     <ChakraProvider>
       <App />
+      <ReactQueryDevtools initialIsOpen= {false} position={'bottom-right'}/>
     </ChakraProvider>
-    
     </QuerClientProvider>
   </React.StrictMode>,
 );
