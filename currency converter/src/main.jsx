5c623953d0ad61611d74cb56853f10724a63922d
ibrahimy353 <ibrahimy353@gmail.com>
2023-02-03 17:32:25 +0300

    added changes

--- currency converter/src/main.jsx
+++ currency converter/src/main.jsx
@@ -6,14 +6,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 const queryClient = new QueryClient();
-
+//queryclient gives us our API calls and also used in state management 
 ReactDOM.createRoot(document.getElementById('root')).render(
  
  <React.StrictMode>
     <QuerClientProvider cleint ={queryClient}>
     <ChakraProvider>
       <App />
-      <ReactQueryDevtools initialIsOpen= {false} position={'bottom-right'}/>
+      <ReactQueryDevtools initialIsOpen= {false} position={"bottom-right"}/>
     </ChakraProvider>
     </QuerClientProvider>
   </React.StrictMode>,
