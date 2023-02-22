8a5d974387a6b285898fe98d4309f37651dcfe1e
ibrahimy353 <ibrahimy353@gmail.com>
2023-02-22 14:13:25 +0300

    added the exchange rate api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import CurrencyRow from './CurrencyRow';
-
+const BASE_URL = 'https://v6.exchangerate-api.com/v6/bae7ff234eccef6ead0e40bb/latest/USD'
 function App() {
-  return (
 
+  useEffect (() =>{
+
+    fetch(BASE_URL)
+    .then( res => res.json())
+    .then(data => console.log(data))
+
+  }, [])
+
+  return (
       <>
       <h1>Currency Convertor</h1>
       <CurrencyRow />
