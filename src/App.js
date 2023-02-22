a1a5402cc52be09fffc11fa7ba460c7c2aeee9c9
ibrahimy353 <ibrahimy353@gmail.com>
2023-02-22 17:06:37 +0300

    modify the api to fetch in currency data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import CurrencyRow from './CurrencyRow';
-const BASE_URL = 'https://v6.exchangerate-api.com/v6/bae7ff234eccef6ead0e40bb/latest/USD'
-function App() {
 
+const BASE_URL = 'https://v6.exchangerate-api.com/v6/bae7ff234eccef6ead0e40bb/latest/USD'
+
+function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([])
+  console.log(currencyOptions)
   useEffect (() =>{
 
     fetch(BASE_URL)
     .then( res => res.json())
-    .then(data => console.log(data))
+    .then(data =>{
+      setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+    })
 
   }, [])
 
   return (
       <>
       <h1>Currency Convertor</h1>
-      <CurrencyRow />
+      <CurrencyRow
+      currencyOptions={currencyOptions}/>
       <div className='equals'>
         =
       </div>
-      <CurrencyRow />
+      <CurrencyRow 
+      currencyOptions={currencyOptions}/>
+      
       </> 
   
   );
