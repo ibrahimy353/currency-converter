6063013df25a1f71ff8aed22d0d3cdd0e96420c1
ibrahimy353 <ibrahimy353@gmail.com>
2023-02-24 23:02:51 +0300

    Ibra

--- src/components/InputAmount.jsx
+++ src/components/InputAmount.jsx
@@ -3,7 +3,7 @@ import { Grid, InputAdornment, TextField } from "@mui/material"
 function InputAmount() {
   return (
     <Grid item xs='12' md> 
-    {/* median md gets to fill the available space thus being responsive */}
+    {/* median md gets to fill the available space thus being responsiveIbra */}
         <TextField 
         label='Amount'
         fullWidth
