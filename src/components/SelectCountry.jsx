fbd4785fb3522c4432968dc5093dffdb66193a0e
ibrahimy353 <ibrahimy353@gmail.com>
2023-02-24 21:36:39 +0300

    added countries in the option section

--- src/components/SelectCountry.jsx
+++ src/components/SelectCountry.jsx
@@ -1,11 +1,20 @@
 import { Autocomplete, Grid, TextField } from "@mui/material"
+import useAxios from "../hooks/useAxios"
 
 function SelectCountry() {
+    const [data] = useAxios("https://restcountries.com/v3.1/all");
+
+    const dataFilter = data.filter(item => "currencies" in item);
+    const dataCountries = dataFilter.map(item =>{
+        return `${item.flag} ${Object.keys(item.currencies)[0]}-${item.name.common}`
+    });
+    console.log(dataCountries)
+
   return (
     <Grid item xs='12' md={3}>
         <Autocomplete
             value='option1'
-            options={['option1','option2']}
+            options={dataCountries}
             renderInput={(params) => <TextField {...params} label='from'/>}
         />
             
