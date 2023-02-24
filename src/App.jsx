ba4895ffab934e6e3151219e14c338a8ebde16fc
ibrahimy353 <ibrahimy353@gmail.com>
2023-02-24 17:58:38 +0300

    data addition

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { Button, Container } from '@mui/material'
+import { Button, Container, Grid, Typography } from '@mui/material'
+import InputAmount from './components/InputAmount'
+import SelectCountry from './components/SelectCountry'
+import SwitchCurrency from './components/SwitchCurrency'
 
 
 function App() {
 
 
   return (
-    <div className="App">
-     <Button>Click</Button>
-    </div>
+<Container maxWidth='md' sx={{background:'#2227'}}>
+    <Typography variant='h5' sx={{marginTop:'2rem'}}>
+      Keep it going Ibra
+    </Typography>
+
+    <Grid Container spacing={2}>
+      <InputAmount />
+      <SelectCountry />
+      <SwitchCurrency />
+      <SelectCountry />
+
+    </Grid>
+</Container>     
+    
   )
 }
 
