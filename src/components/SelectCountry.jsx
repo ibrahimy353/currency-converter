8fe1a92793992694724554e698b9518a055910dc
ibrahimy353 <ibrahimy353@gmail.com>
2023-02-25 17:38:29 +0300

    commit

--- src/components/SelectCountry.jsx
+++ src/components/SelectCountry.jsx
@@ -23,7 +23,7 @@ function SelectCountry  (props)  {
     });
    
   return (
-    <Grid item xs='12' md={3}>
+    <Grid item xs={12} md={3}>
         <Autocomplete
             value={value}
             disableClearable
