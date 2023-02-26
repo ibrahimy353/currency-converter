a39492dfc41f177510030b74019f1a54ce89d668
ibrahimy353 <ibrahimy353@gmail.com>
2023-02-26 16:39:56 +0300

    modify

--- src/context/CurrencyContext.jsx
+++ src/context/CurrencyContext.jsx
@@ -3,8 +3,8 @@ import { createContext, useState } from "react";
 export const CurrencyContext =  createContext();
 
 const CurrencyProvider=({ children  }) => {
-    const [fromCurrency, setFromCurrency] = useState("USD - United States");
-    const [toCurrency, setToCurrency] = useState("AUD - Australia");
+    const [fromCurrency, setFromCurrency] = useState("🇺🇸 USD - United States");
+    const [toCurrency, setToCurrency] = useState("🇦🇺 AUD - Australia");
     const [firstAmount, setFirstAmount] = useState("");
 
   
